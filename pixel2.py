5aae578232e0398f674b49d0d3574c9126d948a2
kpratico <praticok@oregonstate.edu>
2018-12-09 17:43:32 -0800

    removed unused code

--- pixel2.py
+++ pixel2.py
@@ -1,9 +1,53 @@
 import math
-from math import pi
 import numpy as np
 import cv2
-from bresenham import bresenham
 
+
+def bresenham(x0, y0, x1, y1):
+    """Yield integer coordinates on the line from (x0, y0) to (x1, y1).
+    Input coordinates should be integers.
+    The result will contain both the start and the end point.
+    https://pypi.org/project/bresenham/
+    """
+    dx = x1 - x0
+    dy = y1 - y0
+
+    xsign = 1 if dx > 0 else -1
+    ysign = 1 if dy > 0 else -1
+
+    dx = abs(dx)
+    dy = abs(dy)
+
+    if dx > dy:
+        xx, xy, yx, yy = xsign, 0, 0, ysign
+    else:
+        dx, dy = dy, dx
+        xx, xy, yx, yy = 0, ysign, xsign, 0
+
+    D = 2*dy - dx
+    y = 0
+
+    for x in range(dx + 1):
+        yield x0 + x*xx + y*yx, y0 + x*xy + y*yy
+        if D >= 0:
+            y += 1
+            D -= 2*dx
+        D += 2*dy
+
+
+def make_circle(center=(0, 0), r=150, n=200):
+    point = np.array([[int(center[0] + math.cos(np.pi*2*i/n) * r), int(center[1] + math.sin(np.pi*2*i/n) * r)] for i in range(n)])
+    return point
+
+def check_tooclose(coords, line, adj):
+    if abs(line- adj) < 25:
+        return True
+
+def update_image(breList, kitty, imgResult):
+    for x in breList:
+        y, z = x
+        kitty[y, z] = 255
+        imgResult[y, z] = 0
 
 
 imgRadius = 500 
@@ -17,60 +61,28 @@ kitty = cv2.resize(imgCropped, (2*imgRadius + 1, 2*imgRadius + 1))
 cv2.imwrite('./resized.png', kitty)
 
 height, width = kitty.shape[:2]
-previousPins = []
 imgResult = 255 * np.ones((height, width))
-nLines = 700
+nLines = 800
 nPins = 200
 oldPin = 0
-minLoop = 3
 height, width = kitty.shape[0:2]
 x = int(width/2)
 y = int(height/2)
 
-def make_circle(center=(0, 0), r=150, n=200):
-    point = np.array([[int(center[0] + math.cos(np.pi*2*i/n) * r), int(center[1] + math.sin(np.pi*2*i/n) * r)] for i in range(n)])
-    return point
-
-def check_tooclose(coords, line, adj):
-    if abs(line- adj) < 25:
-        return True
-
-# def line_drawn(coords, line, adj):
-#     checkme = line, adj
-#     if checkme in coords:
-#         return True
-
 coords = make_circle(center=(x,y),r=500)
 
+# if you want less local lines
 # for x in coords:
 #     y,z = x
-#     cv2.circle(kitty,(y,z), 65, (255,0,0), -1)
-# for line in coords:
-#     sumpix = list()
-#     for adj in coords:
-#         if check_tooclose(line, adj):
-#             continue
-#         bre = list(bresenham(line[0], line[1], adj[0], adj[1]))
-#         for x in bre:
-#             y, z = x
-#             pixels = list()
-#             if y < height and z < width:
-#                 pixels.append(int(kitty[y, z]))
-#         sumpix.append(int(np.sum(pixels)/len(bre)))
+#     cv2.circle(kitty,(y,z), 55, (255,255,0), -1)
 
-#     if sumpix:
-#         themin = sumpix.index(min(sumpix))
-#         kitty = cv2.line(kitty, tuple(line), tuple(coords[themin]), (255,255,255), 1)
-#         #im = cv2.line(im, tuple(line), tuple(coords[themin]), 15, 1)
 
-# cv2.imwrite('lol.png', kitty)
 line_list = []
-    # Loop over lines until stopping criteria is reached
+
 for line in range(nLines):
     bestLine = 9999999999
     oldCoord = coords[oldPin]
 
-    # Loop over possible lines
     for index in range(1, nPins):
         pin = (oldPin + index) % nPins
         pixels = 0
@@ -88,17 +100,8 @@ for line in range(nLines):
             bestBre = bre
 
     line_list.append((oldPin, bestPin))
-    for x in bestBre:
-        y, z = x
-        kitty[y, z] = 255
-        imgResult[y, z] = 0
-
-    if bestPin == oldPin:
-        print "hi"
-        break
-
-
+    update_image(bestBre, kitty, imgResult)
     oldPin = bestPin
 
-cv2.imwrite('lol6.png', kitty)
-cv2.imwrite('threaded6.png', imgResult)
+cv2.imwrite('lol9.png', kitty)
+cv2.imwrite('threaded9.png', imgResult)
