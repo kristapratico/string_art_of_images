29ff3feab4482965800abecfb25fae5867bfc788
kpratico <praticok@oregonstate.edu>
2018-12-12 17:08:19 -0800

    added main and cleaned up code a bit

--- string_line.py
+++ string_line.py
@@ -5,6 +5,7 @@ import time
 
 
 # https://stackoverflow.com/questions/5186939/algorithm-for-drawing-a-4-connected-line
+# Bresenham-like algorithm returns coords for 4-connected lines
 def bresenham(x0, y0, x1, y1):
     dx = abs(x1 - x0)    # distance to travel in X
     dy = abs(y1 - y0)    # distance to travel in Y
@@ -52,73 +53,79 @@ def show_image(string_art, thread_count, num_lines):
     cv2.imshow('image', string_art)
     cv2.waitKey(1) 
 
-def update_image(pinx, piny, image):
+def update_image(pinX, pinY, image, string_art):
     mask = np.zeros(image.shape[:2], dtype = "uint8")
-    cv2.line(mask, pinx, piny, 255, 1)
-    cv2.line(string_art, pinx, piny, 0, 1)
+    cv2.line(mask, pinX, pinY, 255, 1)
+    cv2.line(string_art, pinX, pinY, 0, 1)
     image = cv2.bitwise_or(image, mask)
 
-    return image # do i need this?
+    return image
+
+def main():
+    start_time = time.time()
+
+    orig_img = cv2.imread('kk.jpg', cv2.IMREAD_GRAYSCALE)
+
+    radius = 500 
+    height, width = orig_img.shape[:2]
+    smaller_dim = min(height, width)
+    y1 = int((height - smaller_dim)/2) 
+    x1 = int((width - smaller_dim)/2) 
+    y2 = y1 + smaller_dim
+    x2 = x1 + smaller_dim
+    crop_img = orig_img[y1:y2, x1:x2]
+    image = cv2.resize(crop_img, (2*radius + 1, 2*radius + 1)) 
 
 
-start_time = time.time()
+    height, width = image.shape[:2]
+    string_art = 255 * np.ones((height, width))
+    num_pins = 200
+    num_lines = 1000
 
-orig_img = cv2.imread('kk.jpg', cv2.IMREAD_GRAYSCALE)
+    center_x = int(width/2)
+    center_y = int(height/2)
 
-radius = 500 
-height, width = orig_img.shape[0:2]
-minEdge= min(height, width)
-topEdge = int((height - minEdge)/2)
-leftEdge = int((width - minEdge)/2)
-crop_img = orig_img[topEdge:topEdge+minEdge, leftEdge:leftEdge+minEdge]
-image = cv2.resize(crop_img, (2*radius + 1, 2*radius + 1)) 
+    pin_list = get_pin_list(center=(center_x,center_y),r=radius, n=num_pins)
 
+    start_pin = 0
+    thread_count = 0
+    print_list = []
 
-height, width = image.shape[:2]
-string_art = 255 * np.ones((height, width))
-num_lines = 1000
-num_pins = 200
-start_pin = 0
+    for line in range(num_lines):
+        darkest_line = float("inf")
+        start_coords = pin_list[start_pin]
 
-center_x = int(width/2)
-center_y = int(height/2)
+        for pin in range(1, num_pins):
+            end_pin = (start_pin + pin) % num_pins
+            end_coords = pin_list[end_pin]
 
-pin_list = get_pin_list(center=(center_x,center_y),r=radius, n=num_pins)
+            if pins_too_close(start_pin, end_pin):
+                continue
 
-print_list = []
-thread_count = 0
+            line_coords = list(bresenham(start_coords[0], start_coords[1], end_coords[0], end_coords[1]))  
+            pinX, pinY = zip(*line_coords)
+            pixel_sum = np.sum(image[pinY, pinX])
+            line_avg = int(pixel_sum / len(line_coords))
+            if line_avg < darkest_line:
+                next_pin = end_pin
+                darkest_line = line_avg
+                
+        thread_count += 1
+        print_list.append((start_pin, next_pin))   
+        image = update_image(tuple(pin_list[start_pin]), tuple(pin_list[next_pin]), image, string_art)
+        #show_image(string_art, thread_count, num_lines)
+        start_pin = next_pin
 
-for line in range(num_lines):
-    best_line = float("inf")
-    start_coords = pin_list[start_pin]
+    cv2.imwrite('kk_results4.png', string_art)
+    print int(time.time() - start_time), "seconds elapsed"
+    # cv2.namedWindow('image', cv2.WINDOW_NORMAL) 
+    # cv2.resizeWindow('image', height, width)
+    # cv2.imshow('image', string_art)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
-    for pin in range(1, num_pins):
-        end_pin = (start_pin + pin) % num_pins
-        end_coords = pin_list[end_pin]
+    # write_file = open("instructions.txt", "w")
+    # write_file.writelines(str(print_list))
 
-        if pins_too_close(start_pin, end_pin):
-            continue
-
-        line_coords = list(bresenham(start_coords[0], start_coords[1], end_coords[0], end_coords[1]))  
-        pinX, pinY = zip(*line_coords)
-        pixel_sum = np.sum(image[pinY, pinX])
-        line_avg = int(pixel_sum / len(line_coords))
-        if line_avg < best_line:
-            best_pin = end_pin
-            best_line = line_avg
-            
-    thread_count += 1
-    print_list.append((start_pin, best_pin))   
-    image = update_image(tuple(pin_list[start_pin]), tuple(pin_list[best_pin]), image)
-    #show_image(string_art, thread_count, num_lines)
-    start_pin = best_pin
-
-cv2.imwrite('kk_results3.png', string_art)
-cv2.namedWindow('image', cv2.WINDOW_NORMAL) 
-cv2.resizeWindow('image', height, width)
-cv2.imshow('image', string_art)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-print int(time.time() - start_time), "seconds elapsed"
-# write_file = open("instructions.txt", "w")
-# write_file.writelines(str(print_list))
+if __name__== "__main__":
+  main()
